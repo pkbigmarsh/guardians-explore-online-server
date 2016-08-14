881f629906740270365b6e9d382570a7449b6409
marshall_sumwalt <marshallsumwalt@gmail.com>
2016-08-13 21:37:05 -0700

    Initial project commit

---
+++ src/main/java/com/guardians/explore/online/CoreApplication.java
@@ -0,0 +1,12 @@
+package com.guardians.explore.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApplication {
+
+	// public static void main(String[] args) {
+	// 	SpringApplication.run(CoreApplication.class, args);
+	// }
+}
